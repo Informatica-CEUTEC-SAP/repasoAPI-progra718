3c41a4ab7cca39a1fb1fc7f0e1bcb4b76b911108
Ivan Cruz <ivancruzhn@gmail.com>
2025-05-28 20:49:07 -0600

    feat: validar ambiente para inyectar seeders

--- repasoAPI/Data/Seeders/EstudiantesSeeders.cs
+++ repasoAPI/Data/Seeders/EstudiantesSeeders.cs
@@ -8,11 +8,18 @@ public class EstudiantesSeeders: IEntityTypeConfiguration<Estudiante>
 {
     public void Configure(EntityTypeBuilder<Estudiante> builder)
     {
-        builder.HasData(
-            new Estudiante { Id = Guid.NewGuid(), Nombre = "Juan Pérez", FechaNacimiento = new DateTime(1982, 3, 25) },
-            new Estudiante { Id = Guid.NewGuid(), Nombre = "Ana Gómez", FechaNacimiento = new DateTime(2020, 1, 11) },
-            new Estudiante { Id = Guid.NewGuid(), Nombre = "Luis Fernández", FechaNacimiento = new DateTime(1999, 4, 12) },
-            new Estudiante { Id = Guid.NewGuid(), Nombre = "María López", FechaNacimiento = new DateTime(1982, 5, 13) }
-        );
+        if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development")
+        {
+            builder.HasData(    
+                new Estudiante
+                    { Id = Guid.NewGuid(), Nombre = "Juan Pérez", FechaNacimiento = new DateTime(1982, 3, 25) },
+                new Estudiante
+                    { Id = Guid.NewGuid(), Nombre = "Ana Gómez", FechaNacimiento = new DateTime(2020, 1, 11) },
+                new Estudiante
+                    { Id = Guid.NewGuid(), Nombre = "Luis Fernández", FechaNacimiento = new DateTime(1999, 4, 12) },
+                new Estudiante
+                    { Id = Guid.NewGuid(), Nombre = "María López", FechaNacimiento = new DateTime(1982, 5, 13) }
+            );
+        }
     }
 }
